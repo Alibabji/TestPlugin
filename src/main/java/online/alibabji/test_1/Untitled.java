3ead23575cb7fe4784071c17994dad8632355957
Alibabji <woodzin070929@gmail.com>
2023-06-09 09:58:20 -0400

    Initial Commit for this project

---
+++ src/main/java/online/alibabji/test_1/Untitled.java
@@ -0,0 +1,19 @@
+package online.alibabji.test_1;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Untitled extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("plugin in now running");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+        System.out.println("plugin in not anymore running");
+    }
+}
