0383845441b188b378bc55902d62436605d0ec0b
Alibabji <woodzin070929@gmail.com>
2023-06-09 10:05:37 -0400

    Initial Commit for this project

--- src/main/java/online/alibabji/test_1/main.java
+++ src/main/java/online/alibabji/test_1/main.java
@@ -2,7 +2,7 @@ package online.alibabji.test_1;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Untitled extends JavaPlugin {
+public final class main extends JavaPlugin {
 
     @Override
     public void onEnable() {
